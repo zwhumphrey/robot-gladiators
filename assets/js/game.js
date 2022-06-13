74b477d444fa6842d9ea357424fc4d39c33c79db
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-06-13 15:58:41 -0500

    added var playerName

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!")
+};
+
+
+// fight();
+
