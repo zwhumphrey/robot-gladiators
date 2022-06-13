24ead501afa6efd9589e02499a7cc23fc13ef4fb
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-06-13 17:09:47 -0500

    added conditional statement to tell user if player or enemy has died

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,59 @@
 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+// you can log multiplue values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-// this creates a function named "fight"
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-  window.alert("The fight has begun!")
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." 
+  );
+
+  // check enemyHealth
+  if (enemyHealth <=0) {
+    window.alert(enemyName + " has died!");
+  } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+
+  // check playHealth
+  if (playerHealth <=0) {
+    window.alert(playerName + " has died!");
+  } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+
 };
+// this displays enemyHealth before the fight function
+// console.log(enemyHealth);
 
+fight();
+
+// this displays enemyHealth after the fight function
+// console.log(enemyHealth);
 
 // fight();
 
+// //this displays enemyHealth after the fight function has happened twice becasue we have called it twice
+// console.log(enemyHealth);
